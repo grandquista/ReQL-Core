afc1fb93e8713feecefeb10e77c725b28f09b5f8
Adam Grandquist <grandquista@gmail.com>
2014-11-15 12:47:09 -0800

    Make Python try to build extension.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup
+from setuptools import setup, Extension
 
 setup(
+    ext_modules=[Extension('libReQL', [
+        'ReQL-expr.c',
+        'ReQL-ast.c',
+        'ReQL.c',
+        'ReQL-expr-Python.c',
+        'ReQL-ast-Python.c',
+        'ReQL-Python.c'
+    ])],
     maintainer='Adam Grandquist',
     maintainer_email='grandquista@gmail.com',
     name='libReQL',
