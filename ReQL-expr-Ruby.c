914d254e37249074e957e6c0cc3fae3dd56d16eb
Adam Grandquist <grandquista@gmail.com>
2014-11-16 21:40:32 -0800

    Start work on Ruby conversion functions.

--- ReQL-expr-Ruby.c
+++ ReQL-expr-Ruby.c
@@ -3,3 +3,81 @@
  */
 
 #include "ReQL-expr-Ruby.h"
+
+static VALUE _reql_rb_expr(int argn, VALUE *args, VALUE self) {
+  rb_Integer(args[1]);
+  const long nesting_depth = 20;
+
+  return _reql_to_rb(_reql_from_rb(args[0], nesting_depth));
+}
+
+static _ReQL_Op_t *_reql_from_rb(VALUE obj, long nesting_depth) {
+  if (nesting_depth <= 0) {
+    return NULL;
+  }
+
+  return NULL;
+}
+
+static VALUE _reql_to_rb(_ReQL_Op_t *query) {
+  VALUE res = NULL;
+  switch (query->dt) {
+    case _REQL_R_ARRAY: {
+      _ReQL_Op_t *iter = _reql_to_array(query);
+      if (!iter) {
+        break;
+      }
+
+      _ReQL_Op_t *elem;
+
+      while (_reql_array_next(&iter, &elem)) {
+        _reql_to_rb(elem);
+      }
+      break;
+    }
+    case _REQL_R_BOOL: {
+      int value;
+      if (_reql_to_bool(query, &value)) {
+        break;
+      }
+      break;
+    }
+    case _REQL_R_JSON: {
+      break;
+    }
+    case _REQL_R_NULL: {
+      break;
+    }
+    case _REQL_R_NUM: {
+      double value;
+      if (_reql_to_number(query, &value)) {
+        break;
+      }
+      break;
+    }
+    case _REQL_R_OBJECT: {
+      _ReQL_Op_t *iter = _reql_to_object(query);
+      if (!iter) {
+        break;
+      }
+
+      _ReQL_Op_t *key;
+      _ReQL_Op_t *val;
+
+      while (_reql_object_next(&iter, &key, &val)) {
+        _reql_to_rb(key);
+        _reql_to_rb(val);
+      }
+      break;
+    }
+    case _REQL_R_STR: {
+      unsigned long str_len;
+      const char *str;
+      if (_reql_to_string(query, &str, &str_len)) {
+        break;
+      }
+      break;
+    }
+  }
+  return res;
+}
--- ReQL-expr-Ruby.h
+++ ReQL-expr-Ruby.h
@@ -9,4 +9,8 @@
 #ifndef _REQL_EXPR_RUBY
 #define _REQL_EXPR_RUBY
 
+static VALUE _reql_rb_expr(int argn, VALUE *args, VALUE self);
+static _ReQL_Op_t *_reql_from_rb(VALUE obj, long nesting_depth);
+static VALUE _reql_to_rb(_ReQL_Op_t *query);
+
 #endif
