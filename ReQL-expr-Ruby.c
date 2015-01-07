a7e54b0cf00c3128159863013befbafca55e6e19
Adam Grandquist <grandquista@gmail.com>
2015-01-07 10:57:22 -0800

    Remove expired Ruby code.

--- ReQL-expr-Ruby.c
+++ ReQL-expr-Ruby.c
@@ -35,63 +35,6 @@ static _ReQL_Op _reql_from_rb(VALUE obj, long nesting_depth) {
 }
 
 static VALUE _reql_to_rb(_ReQL_Op query) {
-  VALUE res = NULL;
-  switch (query->obj.datum.dt) {
-    case _REQL_R_ARRAY: {
-      unsigned long size;
-      if (_reql_to_array_(query, &size)) {
-        break;
-      }
-
-      unsigned long i;
-
-      for (i=0; i<size; ++i) {
-        _reql_to_rb(_reql_array_index(query, i));
-      }
-      break;
-    }
-    case _REQL_R_BOOL: {
-      int value;
-      if (_reql_to_bool(query, &value)) {
-        break;
-      }
-      break;
-    }
-    case _REQL_R_JSON: {
-      break;
-    }
-    case _REQL_R_NULL: {
-      break;
-    }
-    case _REQL_R_NUM: {
-      double value;
-      if (_reql_to_number(query, &value)) {
-        break;
-      }
-      break;
-    }
-    case _REQL_R_OBJECT: {
-      _ReQL_Op iter = _reql_to_object(query);
-      if (!iter) {
-        break;
-      }
-
-      _ReQL_Op key;
-      _ReQL_Op val;
-
-      while (_reql_object_next(&iter, &key, &val)) {
-        _reql_to_rb(key);
-        _reql_to_rb(val);
-      }
-      break;
-    }
-    case _REQL_R_STR: {
-      _ReQL_String_t *str;
-      if (_reql_to_string(query, &str)) {
-        break;
-      }
-      break;
-    }
-  }
+  VALUE res;
   return res;
 }
