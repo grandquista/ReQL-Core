b276ae1a948ab393aa6dc88baa993a1922ab5f10
Adam Grandquist <grandquista@gmail.com>
2015-01-13 19:21:47 -0800

    Add test to retrieve a table from database.

--- ReQL-ast-test.cpp
+++ ReQL-ast-test.cpp
@@ -1,20 +1,36 @@
 #include "ReQL-ast-test.hpp"
 
+using namespace ReQL;
+
 class BlankTest : public ASTTest {
 public:
   BlankTest() : ASTTest() {
   }
 
-  void setup() {
-    setName("BlankTest");
-  }
-
-  void run() {
-  }
+  void setup();
 
   void cleanup() {
+    conn.close();
+  }
+
+  Connection conn;
+};
+
+class TableTermTest : public BlankTest {
+public:
+  TableTermTest() : BlankTest() {};
+
+  void run() {
+    table({expr("test")}).run(conn);
   }
 };
+
+void BlankTest::setup() {
+  setName("BlankTest");
+  conn = connect();
+  assert(conn.isOpen(), "Connection failed.");
+  addTest(TableTermTest());
+}
 
 void ASTTest::setup() {
   setName("ASTTest");
