8378e2cfb05dbdddc57d58b8a6b1ecb04ca9f4cb
Adam Grandquist <grandquista@gmail.com>
2015-01-16 09:44:49 -0800

    Add string c constructor test.

--- ReQL-expr-test.cpp
+++ ReQL-expr-test.cpp
@@ -9,3 +9,20 @@ TEST_CASE("Connection", "[c++][connect]") {
 
   REQUIRE(conn.isOpen());
 }
+
+TEST_CASE("Expr", "[c][expr]") {
+  SECTION("string") {
+    _ReQL_Op_t string;
+
+    const uint32_t size = 12;
+
+    uint8_t buf[size] = "Hello World";
+
+    std::string orig = std::string((char *)buf, size);
+
+    _reql_string_init(&string, buf, size, size);
+
+    CHECK(orig.compare(0, size, (char *)_reql_string_buf(&string), size) == 0);
+    CHECK(size == _reql_string_size(&string));
+  }
+}
