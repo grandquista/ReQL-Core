d7da7f7a2c1e8baeb42fea01c7c9ba159d1786b6
Adam Grandquist <grandquista@gmail.com>
2015-01-16 16:47:57 -0800

    Add c object tests.

--- ReQL-expr-test.cpp
+++ ReQL-expr-test.cpp
@@ -12,6 +12,86 @@ TEST_CASE("Connection", "[c++][connect]") {
   REQUIRE(conn.isOpen());
 }
 
+TEST_CASE("Expr object", "[c][expr][object]") {
+  _ReQL_Op_t obj, key, val;
+
+  _ReQL_Pair_t pair[1];
+
+  _reql_object_init(&obj, pair, 1);
+
+  const uint32_t size = 4;
+
+  uint8_t buf[size] = "key";
+
+  _reql_string_init(&key, buf, size, size);
+
+  _reql_null_init(&val);
+
+  CHECK(_reql_object_add(&obj, &key, &val) == 0);
+
+  SECTION("verify insert") {
+    CHECK(_reql_object_get(&obj, &key) != NULL);
+
+    _ReQL_Op res = _reql_object_get(&obj, &key);
+
+    CHECK(_reql_datum_type(res) == _REQL_R_NULL);
+  }
+
+  SECTION("reuse key") {
+    _ReQL_Op_t new_val;
+
+    const double num = 3.14;
+
+    _reql_number_init(&new_val, num);
+
+    CHECK(_reql_object_add(&obj, &key, &new_val) == 0);
+    CHECK(_reql_object_get(&obj, &key) != NULL);
+
+    _ReQL_Op res = _reql_object_get(&obj, &key);
+
+    CHECK(_reql_datum_type(res) == _REQL_R_NUM);
+    CHECK(_reql_to_number(res) == num);
+  }
+
+  SECTION("duplicate key") {
+    _ReQL_Op_t new_key, new_val;
+
+    const double num = 3.14;
+
+    uint8_t new_buf[size] = "key";
+
+    _reql_string_init(&new_key, new_buf, size, size);
+
+    _reql_number_init(&new_val, num);
+
+    CHECK(_reql_object_add(&obj, &new_key, &new_val) == 0);
+    CHECK(_reql_object_get(&obj, &new_key) != NULL);
+
+    _ReQL_Op res = _reql_object_get(&obj, &new_key);
+
+    CHECK(_reql_datum_type(res) == _REQL_R_NUM);
+  }
+
+  SECTION("out of space") {
+    _ReQL_Op_t new_key, new_val;
+
+    const double num = 3.14;
+
+    uint8_t new_buf[size] = "new";
+
+    _reql_string_init(&new_key, new_buf, size, size);
+
+    _reql_number_init(&new_val, num);
+
+    CHECK(_reql_object_add(&obj, &new_key, &new_val) != 0);
+    CHECK(_reql_object_get(&obj, &new_key) == NULL);
+
+    _ReQL_Op res = _reql_object_get(&obj, &key);
+
+    CHECK(_reql_datum_type(res) == _REQL_R_NULL);
+  }
+}
+
 TEST_CASE("Expr", "[c][expr]") {
   _ReQL_Op_t val;
 
