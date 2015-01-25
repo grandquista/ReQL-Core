f29b2192870d03f87299b85bfd862f077cc7d04a
Adam Grandquist <grandquista@gmail.com>
2015-01-25 14:45:27 -0800

    Add simple object decode test.

--- ReQL-expr-test.cpp
+++ ReQL-expr-test.cpp
@@ -113,6 +113,18 @@ TEST_CASE("decode values", "[c][decode]") {
 
     _reql_json_destroy(obj);
   }
+
+  SECTION("object") {
+    const uint32_t size = 7;
+    uint8_t src[size] = "{\"\":0}";
+
+    _ReQL_Obj_t *obj = _reql_decode(src, size);
+
+    REQUIRE(obj != NULL);
+    REQUIRE(_reql_datum_type(obj) == _REQL_R_STR);
+
+    _reql_json_destroy(obj);
+  }
 }
 
 TEST_CASE("Expr array", "[c][expr][array]") {
