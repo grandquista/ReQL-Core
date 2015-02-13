8d4a5b0e2266ecd359b04be813a9f9d306961055
Adam Grandquist <grandquista@gmail.com>
2015-02-13 08:41:16 -0800

    Import blank header for consistency.

--- ReQL-expr.c
+++ ReQL-expr.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 Adam Grandquist
+Copyright 2014-2015 Adam Grandquist
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,3 +17,5 @@ limitations under the License.
  * @author Adam Grandquist
  * @copyright Apache
  */
+
+#include "ReQL-expr.h"
