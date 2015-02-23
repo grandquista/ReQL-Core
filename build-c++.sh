2536003d3a8bdf8221b00fce4cbb8c6b2aaf4bc9
Adam Grandquist <grandquista@gmail.com>
2015-02-22 18:33:21 -0800

    Test a simpler c++ build script.

--- build-c++.sh
+++ build-c++.sh
@@ -1,11 +1,5 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-4.8 g++-4.8
-sudo update-alternatives --remove-all gcc
-sudo update-alternatives --remove-all g++
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
-make
-make libReQLtest
+CC=gcc-4.8 CXX=g++-4.8 make
+CC=gcc-4.8 CXX=g++-4.8 make libReQLtest
