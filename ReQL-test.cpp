f980e01718367a53aa817d66497c2964b3be38d4
Adam Grandquist <grandquista@gmail.com>
2015-03-03 13:00:08 -0800

    Move variable declaration inside switch.

--- ReQL-test.cpp
+++ ReQL-test.cpp
@@ -8,10 +8,10 @@ std::string inspect(ReQL::ReQL_Obj_t *query) {
     return "unknown";
   }
 
-  std::stringstream buf;
-
   switch (ReQL::reql_datum_type(query)) {
     case ReQL::REQL_R_ARRAY: {
+      std::stringstream buf;
+      
       buf << "[";
 
       ReQL::ReQL_Iter_t it = ReQL::reql_new_iter(query);
@@ -27,16 +27,16 @@ std::string inspect(ReQL::ReQL_Obj_t *query) {
       }
 
       buf << "]";
-      break;
+
+      return buf.str();
     }
     case ReQL::REQL_R_BOOL: return ReQL::reql_to_bool(query) ? "true" : "false";
     case ReQL::REQL_R_JSON: return "error";
     case ReQL::REQL_R_NULL: return "null";
-    case ReQL::REQL_R_NUM: {
-      buf << ReQL::reql_to_number(query);
-      break;
-    }
+    case ReQL::REQL_R_NUM: return std::to_string(ReQL::reql_to_number(query));
     case ReQL::REQL_R_OBJECT: {
+      std::stringstream buf;
+
       buf << "{";
 
       ReQL::ReQL_Iter_t it = ReQL::reql_new_iter(query);
@@ -54,9 +54,12 @@ std::string inspect(ReQL::ReQL_Obj_t *query) {
       }
 
       buf << "}";
-      break;
+
+      return buf.str();
     }
     case ReQL::REQL_R_REQL: {
+      std::stringstream buf;
+
       buf << "(";
       buf << ReQL::reql_datum_type(query);
       ReQL::ReQL_Obj_t *args = ReQL::reql_args(query);
@@ -70,10 +73,11 @@ std::string inspect(ReQL::ReQL_Obj_t *query) {
         buf << inspect(kwargs);
       }
       buf << ")";
-      break;
+
+      return buf.str();
     }
     case ReQL::REQL_R_STR: return std::string(reinterpret_cast<char *>(ReQL::reql_string_buf(query)), ReQL::reql_size(query));
   }
 
-  return buf.str();
+  throw;
 }
