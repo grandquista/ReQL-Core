6d9e1d6d916a90fe39cf43ba2072725c5acc3b08
Adam Grandquist <grandquista@gmail.com>
2015-05-25 10:47:09 -0700

    Add objective c framework.

---
+++ libReQL/libReQL.h
@@ -0,0 +1,19 @@
+//
+//  libReQL.h
+//  libReQL
+//
+//  Created by Adam Grandquist on 5/25/15.
+//  Copyright (c) 2015 Adam Grandquist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for libReQL.
+FOUNDATION_EXPORT double libReQLVersionNumber;
+
+//! Project version string for libReQL.
+FOUNDATION_EXPORT const unsigned char libReQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libReQL/PublicHeader.h>
+
+
