8f679ef8b2b5f2ad09e4107d18456770aae68c12
Adam Grandquist <grandquista@gmail.com>
2015-05-30 21:34:20 -0700

    Remove fake tests.

--- libReQLTests/Connection.m
+++ libReQLTests/Connection.m
@@ -18,24 +18,15 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
   ReQLConnection *connection = [ReQLConnection new];
   XCTAssert([connection isOpen]);
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
