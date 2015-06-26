529ce14c3b930a8fef8ffca582ce8a1a62f2fb9f
Adam Grandquist <grandquista@gmail.com>
2015-06-26 11:58:52 -0700

    Add more cursor tests.

--- test/reql/cursor.cpp
+++ test/reql/cursor.cpp
@@ -7,15 +7,33 @@
 
 static void
 reql_test_cur_end_cb(void *data) {
-  int *num = static_cast<int*>(data);
-  *num = 0;
+  *static_cast<int*>(data) = 0;
 }
 
 static void
 reql_test_cur_error_cb(ReQL_Obj_t *res, void *data) {
   (void)res;
+  *static_cast<int*>(data) = 0;
+}
+
+static int
+reql_test_cur_each_cb(ReQL_Obj_t *res, void *data) {
+  if (res != nullptr) {
+    ++(*static_cast<int*>(data));
+  }
+  return res == nullptr;
+}
+
+static int
+reql_test_cur_each_inf_cb(ReQL_Obj_t *res, void *data) {
   int *num = static_cast<int*>(data);
-  *num = 0;
+  if (res != nullptr) {
+    ++(*num);
+  }
+  if (*num > 99) {
+    return 1;
+  }
+  return 0;
 }
 
 TEST_CASE("reql cursor", "[reql][cursor]") {
@@ -33,43 +51,129 @@ TEST_CASE("reql cursor", "[reql][cursor]") {
 
   std::unique_ptr<ReQL_Cur_t> c(new ReQL_Cur_t);
 
-  reql_run(c.get(), nullptr, conn.get(), nullptr);
+  SECTION("basic") {
+    ReQL_Size size = 1;
 
-  REQUIRE(reql_cur_open(c.get()) != 0);
+    ReQL_Obj_t *args = new ReQL_Obj_t;
+    ReQL_Obj_t **array = new ReQL_Obj_t*[size];
+    reql_array_init(args, array, size);
 
-  SECTION("reql_cur_set_end_cb") {
+    ReQL_Obj_t *elem = new ReQL_Obj_t;
+    reql_number_init(elem, 100);
+    reql_array_append(args, elem);
+
+    ReQL_Obj_t *query = new ReQL_Obj_t;
+    reql_ast_range(query, args);
+
+    REQUIRE(reql_run(c.get(), query, conn.get(), nullptr) == 0);
+
+    reql_json_destroy(query);
+
+    REQUIRE(reql_cur_open(c.get()) != 0);
+
+    SECTION("reql_cur_set_end_cb") {
+      int data = 1;
+      reql_cur_set_end_cb(c.get(), reql_test_cur_end_cb, &data);
+      reql_cur_drain(c.get());
+      REQUIRE(data == 0);
+    }
+
+    SECTION("reql_cur_destroy") {
+      reql_cur_destroy(c.get());
+
+      REQUIRE(reql_cur_open(c.get()) == 0);
+    }
+
+    SECTION("reql_cur_close") {
+      reql_cur_close(c.get());
+
+      REQUIRE(reql_cur_open(c.get()) == 0);
+    }
+
+    SECTION("reql_cur_next") {
+      int data = 0;
+      ReQL_Obj_t *res = nullptr;
+      while ((res = reql_cur_next(c.get())) != nullptr) {
+        REQUIRE(reql_datum_type(res) == REQL_R_NUM);
+        REQUIRE(reql_to_number(res) == Approx(data));
+        reql_json_destroy(res);
+        ++data;
+      }
+      REQUIRE(reql_cur_open(c.get()) == 0);
+    }
+
+    SECTION("reql_cur_each") {
+      int data = 0;
+      reql_cur_each(c.get(), reql_test_cur_each_cb, &data);
+      reql_cur_drain(c.get());
+      REQUIRE(data == 100);
+      REQUIRE(reql_cur_open(c.get()) == 0);
+    }
+  }
+
+  SECTION("infinite") {
+    ReQL_Obj_t *query = new ReQL_Obj_t;
+    reql_ast_range(query, nullptr);
+
+    REQUIRE(reql_run(c.get(), query, conn.get(), nullptr) == 0);
+
+    reql_json_destroy(query);
+
+    REQUIRE(reql_cur_open(c.get()) != 0);
+    
+    SECTION("reql_cur_next") {
+      int data = 0;
+      ReQL_Obj_t *res = nullptr;
+      for (; (res = reql_cur_next(c.get())) != nullptr && data < 100; ++data) {
+        REQUIRE(reql_datum_type(res) == REQL_R_NUM);
+        REQUIRE(reql_to_number(res) == Approx(data));
+        reql_json_destroy(res);
+      }
+      REQUIRE(data == 100);
+      REQUIRE(reql_cur_open(c.get()) != 0);
+      reql_cur_close(c.get());
+      REQUIRE(reql_cur_open(c.get()) == 0);
+    }
+
+    SECTION("reql_cur_each") {
+      int data = 0;
+      reql_cur_each(c.get(), reql_test_cur_each_inf_cb, &data);
+      reql_cur_drain(c.get());
+      REQUIRE(data == 100);
+      REQUIRE(reql_cur_open(c.get()) == 0);
+    }
+  }
+
+  SECTION("reql_cur_set_error_cb") {
+    std::unique_ptr<ReQL_Obj_t> query(new ReQL_Obj_t);
+    std::unique_ptr<ReQL_Byte> buf(new ReQL_Byte[2]);
+    const ReQL_Byte src[] = "\0t";
+    reql_string_init(query.get(), buf.get(), src, 2);
+    reql_run(c.get(), query.get(), conn.get(), nullptr);
+
+    REQUIRE(reql_cur_open(c.get()) != 0);
+
     int data = 1;
-    reql_cur_set_end_cb(c.get(), reql_test_cur_end_cb, &data);
+    reql_cur_set_error_cb(c.get(), reql_test_cur_error_cb, &data);
     reql_cur_drain(c.get());
     REQUIRE(data == 0);
   }
 
-  SECTION("reql_cur_set_error_cb") {
-    int data = 1;
-    reql_cur_set_error_cb(c.get(), reql_test_cur_error_cb, &data);
-    reql_cur_drain(c.get());
-    REQUIRE(data == 1);
-  }
-
-  SECTION("reql_cur_destroy") {
-    reql_cur_destroy(c.get());
-
-    REQUIRE(reql_cur_open(c.get()) == 0);
-  }
-
-  SECTION("reql_cur_next") {
-  }
-
-  SECTION("reql_cur_each") {
-  }
-  
   SECTION("reql_cur_to_array") {
-  }
-  
-  SECTION("reql_cur_close") {
-    reql_cur_close(c.get());
+    ReQL_Size size = 1;
 
-    REQUIRE(reql_cur_open(c.get()) == 0);
+    ReQL_Obj_t *args = new ReQL_Obj_t;
+    ReQL_Obj_t **array = new ReQL_Obj_t*[size];
+    reql_array_init(args, array, size);
+
+    ReQL_Obj_t *elem = new ReQL_Obj_t;
+    reql_number_init(elem, 100);
+    reql_array_append(args, elem);
+
+    ReQL_Obj_t *query = new ReQL_Obj_t;
+    reql_ast_range(query, args);
+
+    REQUIRE(reql_run(c.get(), query, conn.get(), nullptr) == 0);
   }
 
   reql_cur_close(c.get());
