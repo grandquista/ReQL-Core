3681cdfb15c8d39bb9742c52e8d69d66a82f40e1
Adam Grandquist <grandquista@gmail.com>
2015-11-20 06:09:16 -0800

    Use stl queue class.

--- src/reql/pipe.hpp
+++ src/reql/pipe.hpp
@@ -27,51 +27,10 @@ limitations under the License.
 #include <functional>
 #include <memory>
 #include <mutex>
+#include <queue>
 #include <thread>
 
 namespace _ReQL {
-
-template <class elem_t>
-class Queue_t {
-public:
-  class Buffer_t {
-  public:
-    static const int size = 10;
-
-    Buffer_t *push(elem_t &&value) {
-      if (idx.load() >= size) {
-      }
-      buffer[idx++] = std::move(value);
-      return this;
-    }
-
-    elem_t buffer[size];
-    std::atomic<int> idx;
-    std::atomic<Buffer_t *> next;
-  };
-
-  void push(elem_t &&value) {
-    head->push(std::move(value));
-  }
-
-  bool empty() {
-    return !buffer.idx.load();
-  }
-
-  void pop(elem_t &ref) {
-    auto idx = buffer.idx--;
-    if (idx < 0) {
-      Buffer_t *next = buffer.next.exchange(nullptr);
-      if (next) {
-        buffer = std::move(*next);
-      }
-    }
-    ref = std::move(buffer.buffer[idx]);
-  }
-
-  Buffer_t buffer;
-  Buffer_t *head;
-};
 
 template <class elem_t>
 class Pipe_t;
@@ -98,9 +57,9 @@ public:
   }) {}
 
   template <class func_t, class input_t>
-  Static_Pipe_t(func_t func, Pipe_t<input_t> pipe) : Static_Pipe_t([func, pipe] {
+  Static_Pipe_t(func_t func, std::shared_ptr<Static_Pipe_t<input_t>> &pipe) : Static_Pipe_t([func, pipe] {
     input_t res;
-    pipe >> res;
+    pipe->pop(res);
     return func(std::move(res));
   }) {}
 
@@ -162,14 +121,15 @@ public:
         return !p_queue.empty();
       });
     }
-    p_queue.pop(value);
+    value = p_queue.back();
+    p_queue.pop();
     return *this;
   }
 
   std::condition_variable_any p_cond;
   char p_flag;
   std::mutex p_mutex;
-  Queue_t<elem_t> p_queue;
+  std::queue<elem_t> p_queue;
   std::thread p_thread;
 };
 
@@ -178,15 +138,33 @@ class Pipe_t {
 public:
   Pipe_t() : p_pipe(new Static_Pipe_t<elem_t>) {}
 
+  template <class func_t, class input_t>
+  Pipe_t(func_t func, Pipe_t<input_t> &other) : p_pipe(new Static_Pipe_t<elem_t>(func, other.p_pipe)) {}
+
+  Pipe_t(const Pipe_t &other) : p_pipe(other.p_pipe) {}
+
+  Pipe_t(Pipe_t &&other) : p_pipe(std::move(other.p_pipe)) {}
+
   template <class func_t>
   Pipe_t(func_t func) : p_pipe(new Static_Pipe_t<elem_t>(func)) {}
+
+  Pipe_t &operator =(const Pipe_t &other) {
+    if (this != &other) {
+      p_pipe = other.p_pipe;
+    }
+    return *this;
+  }
+
+  Pipe_t &operator =(Pipe_t &&other) {
+    if (this != &other) {
+      p_pipe = std::move(other.p_pipe);
+    }
+    return *this;
+  }
 
   class Sink_t {
   public:
     Sink_t() {}
-
-    template <class func_t>
-    Sink_t(func_t func, Pipe_t<elem_t> &pipe) : Sink_t(func, pipe.p_pipe) {}
 
     template <class func_t>
     Sink_t(func_t func, std::shared_ptr<Static_Pipe_t<elem_t>> &pipe) : p_thread([func, pipe] {
@@ -201,11 +179,19 @@ public:
 
     ~Sink_t() { p_thread.join(); }
 
+    Sink_t &operator =(Sink_t &&other) {
+      if (this != &other) {
+        p_thread = std::move(other.p_thread);
+      }
+      return *this;
+    }
+
     std::thread p_thread;
   };
 
   template <class func_t>
-  Sink_t sink() {
+  Sink_t sink(func_t func) {
+    return Sink_t(func, p_pipe);
   }
 
   Pipe_t &operator <<(elem_t &&value) {
@@ -220,6 +206,10 @@ public:
 
   bool isOpen() const {
     return !p_pipe->closed();
+  }
+
+  void close() {
+    return p_pipe->close();
   }
 
   std::shared_ptr<Static_Pipe_t<elem_t>> p_pipe;
