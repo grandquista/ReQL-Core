9b15a84643ccb55dc1db7bbdde1d31bb70025f30
Adam Grandquist <grandquista@gmail.com>
2015-11-20 11:53:22 -0800

    Rename to show all instances are immutable.

--- src/reql/string.hpp
+++ src/reql/string.hpp
@@ -32,54 +32,60 @@ limitations under the License.
 namespace _ReQL {
 
 template <class char_t, class Traits = std::char_traits<char_t>, class Allocator = std::allocator<char_t>>
-class _String {
+class BasicImmutableString {
 public:
   typedef Traits traits_type;
   typedef typename traits_type::char_type value_type;
   typedef Allocator allocator_type;
   typedef typename std::allocator_traits<allocator_type>::size_type size_type;
   typedef typename std::allocator_traits<allocator_type>::difference_type difference_type;
-  typedef value_type& reference;
   typedef const value_type& const_reference;
-  typedef typename std::allocator_traits<Allocator>::pointer pointer;
   typedef typename std::allocator_traits<Allocator>::const_pointer const_pointer;
-  typedef pointer iterator;
   typedef const_pointer const_iterator;
 
-  _String() {}
+  BasicImmutableString() {}
 
-  _String(const value_type value) : p_size(1), p_value(new value_type[1]) {
+  BasicImmutableString(const value_type value) : p_size(1), p_value(new value_type[1]) {
     *p_value = value;
   }
 
-  _String(const value_type *value) : p_size(traits_type::length(value) - 1), p_value(new value_type[p_size]) {
+  BasicImmutableString(const_pointer value) : p_size(traits_type::length(value) - 1), p_value(new value_type[p_size]) {
     memcpy(p_value.load(), value);
   }
 
-  _String(const value_type *value, const size_type size) : p_size(size), p_value(new value_type[size]) {
+  BasicImmutableString(const_pointer value, const size_type size) : p_size(size), p_value(new value_type[size]) {
     memcpy(p_value.load(), value);
   }
 
-  _String(const _String &other) : p_size(other.p_size), p_value(new value_type[other.p_size]) {
+  BasicImmutableString(BasicImmutableString &&other) : p_size(other.p_size), p_value(other.p_value.exchange(nullptr)) {}
+
+  BasicImmutableString(const BasicImmutableString &other) : p_size(other.size()), p_value(new value_type[p_size]) {
     memcpy(p_value.load(), other.c_str());
   }
 
-  _String(_String &&other) : p_size(other.p_size), p_value(other.p_value.exchange(nullptr)) {}
-
   template <class str_t>
-  _String(const str_t &str) : p_size(str.size()), p_value(new value_type[p_size]) {
-    memcpy(p_value.load(), str.c_str());
+  BasicImmutableString(const str_t &other) : p_size(other.size()), p_value(new value_type[p_size]) {
+    memcpy(p_value.load(), other.c_str());
   }
 
-  _String &operator =(const _String &other) {
+  BasicImmutableString &operator =(const BasicImmutableString &other) {
     if (this != &other) {
-      p_size = other.p_size;
+      p_size = other.size();
       memcpy(other.c_str());
     }
     return *this;
   }
 
-  _String &operator =(_String &&other) {
+  template <class str_t>
+  BasicImmutableString &operator =(const str_t &other) {
+    if (this != &other) {
+      p_size = other.size();
+      memcpy(other.c_str());
+    }
+    return *this;
+  }
+
+  BasicImmutableString &operator =(BasicImmutableString &&other) {
     if (this != &other) {
       p_size = std::move(other.p_size);
       exchange(other.p_value.exchange(nullptr));
@@ -87,7 +93,7 @@ public:
     return *this;
   }
 
-  ~_String() {
+  ~BasicImmutableString() {
     exchange(nullptr);
   }
 
@@ -95,8 +101,8 @@ public:
     return !p_size;
   }
 
-  _String substr(const size_type pos) const {
-    return _String(p_value.load() + pos, p_size - pos);
+  BasicImmutableString substr(const size_type pos) const {
+    return BasicImmutableString(p_value.load() + pos, p_size - pos);
   }
 
   size_type size() const { return p_size; }
@@ -109,25 +115,11 @@ public:
 
   value_type *data() const { return p_value.load(); }
 
-  reference at(const size_type pos) {
-    if (pos >= size()) {
-      throw std::out_of_range("");
-    }
-    return p_value.load()[pos];
-  }
-
   const_reference at(const size_type pos) const {
     if (pos >= size()) {
       throw std::out_of_range("");
     }
     return p_value.load()[pos];
-  }
-
-  reference operator [](const size_type pos) {
-    if (pos == size()) {
-      return value_type();
-    }
-    return at(pos);
   }
 
   const_reference operator [](const size_type pos) const {
@@ -137,27 +129,15 @@ public:
     return at(pos);
   }
 
-  bool operator <(const _String &other) const {
+  bool operator <(const BasicImmutableString &other) const {
     return p_value < other.p_value;
-  }
-
-  char_t &front() {
-    return p_value.load();
   }
 
   const char_t &front() const {
     return p_value.load();
   }
 
-  iterator begin() {
-    return p_value.load();
-  }
-
   const_iterator cbegin() const {
-    return p_value.load();
-  }
-
-  iterator end() {
     return p_value.load();
   }
 
@@ -174,15 +154,15 @@ private:
     return value;
   }
 
-  void memcpy(const value_type *in) {
+  void memcpy(const_pointer in) {
     memcpy(exchange(new value_type[p_size]), in, p_size);
   }
 
-  void memcpy(value_type *out, const value_type *in) const {
+  void memcpy(value_type *out, const_pointer in) const {
     memcpy(out, in, p_size);
   }
 
-  void memcpy(value_type *out, const value_type *in, const size_type size) const {
+  void memcpy(value_type *out, const_pointer in, const size_type size) const {
     std::memcpy(out, in, sizeof(value_type) * size);
   }
 
@@ -191,14 +171,14 @@ private:
 };
 
 template <class value_type>
-bool operator ==(const _String<value_type> self, const value_type *other) {
+bool operator ==(const BasicImmutableString<value_type> &self, const value_type *other) {
   if (std::char_traits<value_type>::length(other) - 1 == self.size()) {
     return !std::memcmp(self.data(), other, sizeof(value_type) * self.size());
   }
   return false;
 }
 
-typedef _String<ReQL_Byte> ImmutableString;
+typedef BasicImmutableString<ReQL_Byte> ImmutableString;
 
 }  // namespace _ReQL
 
