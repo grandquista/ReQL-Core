b3abacd083ad6967816a78bc05c2ab608cf610c6
Adam Grandquist <grandquista@gmail.com>
2015-11-21 11:10:50 -0800

    Distribute cmake instructions.

--- build.py
+++ build.py
@@ -4,6 +4,39 @@ import scripting
 
 from pathlib import Path
 
+def gather_c_files(path, *subs):
+    cfiles = list()
+    hfiles = list()
+    for sub in subs:
+        cfiles.extend((path / sub).glob('**/*.c'))
+        cfiles.extend((path / sub).glob('**/*.cpp'))
+        hfiles.extend((path / sub).glob('**/*.h'))
+        hfiles.extend((path / sub).glob('**/*.hpp'))
+    return sorted(f.relative_to(path).as_posix() for f in cfiles), sorted(f.relative_to(path).as_posix() for f in hfiles)
+
+def write_cmake(path, cextras, hextras):
+    cmake_list_path = path / 'CMakeLists.txt'
+
+    with cmake_list_path.open() as istream:
+        src = istream.read()
+
+    sources, headers = gather_c_files(path, 'c', 'cpp', 'reql')
+
+    src = re.sub(
+        'set\(MOC_HEADERS.*?\)',
+        lambda m: 'set(MOC_HEADERS\n {}\n)'.format(
+            '\n '.join(headers + hextras)),
+        re.sub(
+            'set\(SOURCES.*?\)',
+            lambda m: 'set(SOURCES\n {}\n)'.format(
+                '\n '.join(sources + cextras)),
+            src, 1, re.S),
+        1, re.S)
+
+    with cmake_list_path.open('w') as ostream:
+        ostream.write(src)
+
+
 def main():
     cwd_path = Path('.')
 
@@ -31,57 +64,8 @@ def main():
 
     scripting.assert_c_header_extern_c(libreql_src_path.glob('**/*.h'))
 
-    headers = list((libreql_src_path / 'c').glob('**/*.h'))
-    headers.extend((libreql_src_path / 'cpp').glob('**/*.h'))
-    headers.extend((libreql_src_path / 'reql').glob('**/*.h'))
-    headers.extend((libreql_src_path / 'c').glob('**/*.hpp'))
-    headers.extend((libreql_src_path / 'cpp').glob('**/*.hpp'))
-    headers.extend((libreql_src_path / 'reql').glob('**/*.hpp'))
-    headers.extend((libreql_test_path / 'c').glob('**/*.h'))
-    headers.extend((libreql_test_path / 'cpp').glob('**/*.h'))
-    headers.extend((libreql_test_path / 'reql').glob('**/*.h'))
-    headers.extend((libreql_test_path / 'c').glob('**/*.hpp'))
-    headers.extend((libreql_test_path / 'cpp').glob('**/*.hpp'))
-    headers.extend((libreql_test_path / 'reql').glob('**/*.hpp'))
-    headers.extend([libreql_src_path / 'ReQL.h', libreql_src_path / 'ReQL.hpp'])
-
-    headers = [path.relative_to(cwd_path).as_posix() for path in headers]
-    headers = sorted(headers)
-
-    sources = list((libreql_src_path / 'c').glob('**/*.c'))
-    sources.extend((libreql_src_path / 'cpp').glob('**/*.c'))
-    sources.extend((libreql_src_path / 'reql').glob('**/*.c'))
-    sources.extend((libreql_src_path / 'c').glob('**/*.cpp'))
-    sources.extend((libreql_src_path / 'cpp').glob('**/*.cpp'))
-    sources.extend((libreql_src_path / 'reql').glob('**/*.cpp'))
-    sources.extend((libreql_test_path / 'c').glob('**/*.c'))
-    sources.extend((libreql_test_path / 'cpp').glob('**/*.c'))
-    sources.extend((libreql_test_path / 'reql').glob('**/*.c'))
-    sources.extend((libreql_test_path / 'c').glob('**/*.cpp'))
-    sources.extend((libreql_test_path / 'cpp').glob('**/*.cpp'))
-    sources.extend((libreql_test_path / 'reql').glob('**/*.cpp'))
-    sources.extend([libreql_test_path / 'main.cpp'])
-
-    sources = sorted(path.relative_to(cwd_path).as_posix() for path in sources)
-
-    cmake_list_path = cwd_path / 'CMakeLists.txt'
-
-    with cmake_list_path.open() as istream:
-        src = istream.read()
-
-    src = re.sub(
-        'set\(MOC_HEADERS.*?\)',
-        lambda m: 'set(MOC_HEADERS\n {}\n)'.format(
-            '\n '.join(headers)),
-        re.sub(
-            'set\(SOURCES.*?\)',
-            lambda m: 'set(SOURCES\n {}\n)'.format(
-                '\n '.join(sources)),
-            src, 1, re.S),
-        1, re.S)
-
-    with cmake_list_path.open('w') as ostream:
-        ostream.write(src)
+    write_cmake(libreql_src_path, [], ['ReQL.h', 'ReQL.hpp'])
+    write_cmake(libreql_test_path, ['main.cpp'], ['catch.hpp'])
 
     objc_src_path = cwd_path / 'libReQL'
 
