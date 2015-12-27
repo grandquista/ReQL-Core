eebdf6c4589bb5e918fbcab468020270d08815a1
Adam Grandquist <grandquista@gmail.com>
2015-12-26 20:40:55 -0800

    Make wide error strings easier to handle.

--- src/core/error.hpp
+++ src/core/error.hpp
@@ -28,23 +28,66 @@ namespace _ReQL {
 enum Error_e {
   CompileError,
   DriverError,
-  RuntimeError,
+  RuntimeError
 };
 
 struct Error_t : public std::exception {
-  Error_t(const Error_e code, const char *errstr) noexcept :
-    _code(code),
-    _what(errstr) {}
+  Error_t(const char *errstr) noexcept;
 
-  Error_t(const Error_e code, const wchar_t *errstr) noexcept :
-    _code(code),
-    _what(reinterpret_cast<const char *>(errstr)) {}
+  Error_t(const Error_e code, const char *errstr) noexcept;
 
-  virtual const char* what() const noexcept override { return _what; }
+  Error_t(const wchar_t *errstr) noexcept;
+  
+  Error_t(const Error_e code, const wchar_t *errstr) noexcept;
 
-  const Error_e _code;
+  virtual const char* what() const noexcept override;
+
+  const wchar_t* wwhat() const noexcept;
+
+  Error_e code() const noexcept;
+
+  bool wide() const noexcept;
+
+  const Error_e _code = DriverError;
   const char *_what;
+  const bool _wide = false;
 };
+
+Error_t::Error_t(const char *errstr) noexcept :
+  _what(errstr) {}
+
+Error_t::Error_t(const Error_e code, const char *errstr) noexcept :
+  _code(code),
+  _what(errstr) {}
+
+Error_t::Error_t(const wchar_t *errstr) noexcept :
+  _what(reinterpret_cast<const char *>(errstr)),
+  _wide(true) {}
+  
+Error_t::Error_t(const Error_e code, const wchar_t *errstr) noexcept :
+  _code(code),
+  _what(reinterpret_cast<const char *>(errstr)),
+  _wide(true) {}
+
+const char *
+Error_t::what() const noexcept {
+  return _wide ? "unhandled wide error" : _what;
+}
+
+const wchar_t *
+Error_t::wwhat() const noexcept {
+  return _wide ? reinterpret_cast<const wchar_t *>(_what) : L"bad wide error";
+}
+
+Error_e
+Error_t::code() const noexcept {
+  return _code;
+}
+  
+bool
+Error_t::wide() const noexcept {
+  return _wide;
+}
 
 }  // namespace _ReQL
 
